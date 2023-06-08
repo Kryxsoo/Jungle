965f00c7708a379ccaa496d13f77de5f7c98f59b
Mathew Charm <mattcharmz@gmail.com>
2023-06-08 16:13:52 -0700

    Fixed User database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,9 @@ class UsersController < ApplicationController
         redirect_to '/signup'
       end
   end
-  
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end   
 
 end
